97d9a2f0785ec05ea1b8292d9118903f5305fce0
Max Clark <maxwelleclark25@gmail.com>
2026-05-10 16:07:48 -0500

    Updating the way deployment / startup works again
    - Change migrations and admin creation back to startup. Keep collect static in deployment.

--- startup.sh
+++ startup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+echo "Applying migrations"
+python manage.py migrate --noinput --settings=adopt_a_tree.settings_azure
+
+echo "Creating/updating admin user"
+python manage.py create_admin --settings=adopt_a_tree.settings_azure
+
 echo "Starting gunicorn server"
 gunicorn --bind=0.0.0.0:8000 --workers 1 --timeout 120 adopt_a_tree.wsgi:application
