fc93ca0456a79c71013221abe6932ba7af86d077
Max Clark <maxwelleclark25@gmail.com>
2026-05-11 12:03:11 -0500

    Speed up startup script to beat container timeout

--- startup.sh
+++ startup.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-echo "Collect static files"
-python manage.py collectstatic --noinput --settings=adopt_a_tree.settings_azure
-
-echo "Applying migrations"
-python manage.py migrate --noinput --settings=adopt_a_tree.settings_azure
-
-echo "Creating/updating admin user"
-python manage.py create_admin --settings=adopt_a_tree.settings_azure
-
 echo "Starting gunicorn server"
 gunicorn --bind=0.0.0.0:8000 --workers 1 --timeout 120 adopt_a_tree.wsgi:application
