878698fc4b4b51c6589af69a3e1db8378c88f3b9
Max Clark <97877032+clarkm25@users.noreply.github.com>
2026-07-09 15:09:58 -0700

    Add static collection to startup script to try and fix collectstatic bug

--- startup.sh
+++ startup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --settings=adopt_a_tree.settings_azure
+
 echo "Starting gunicorn server"
 gunicorn --bind=0.0.0.0:8000 --workers 1 --timeout 120 adopt_a_tree.wsgi:application
